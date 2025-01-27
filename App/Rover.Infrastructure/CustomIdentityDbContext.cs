18319ad4bfa5b07ad7b175bff59d09f290bc2d9e
Zfex <55106553+Zfex@users.noreply.github.com>
2025-01-27 13:17:39 +0100

    Add files via upload

---
+++ App/Rover.Infrastructure/CustomIdentityDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Domain;
+using Microsoft.AspNetCore.Identity;
+
+namespace Rover.Infrastructure.Data
+{
+    public class CustomIdentityDbContext : IdentityDbContext<userData>
+    {
+        public CustomIdentityDbContext(DbContextOptions<CustomIdentityDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // Укажите название таблицы для модели userData
+            builder.Entity<userData>().ToTable("userData");
+
+            // Если нужно, можно настроить другие таблицы Identity
+            builder.Entity<IdentityRole>().ToTable("Roles");
+            builder.Entity<IdentityUserRole<string>>().ToTable("UserRoles");
+            builder.Entity<IdentityUserClaim<string>>().ToTable("UserClaims");
+            builder.Entity<IdentityUserLogin<string>>().ToTable("UserLogins");
+            builder.Entity<IdentityRoleClaim<string>>().ToTable("RoleClaims");
+            builder.Entity<IdentityUserToken<string>>().ToTable("UserTokens");
+        }
+    }
+}
