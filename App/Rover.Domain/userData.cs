1cfdbf715957c76d7387779a10c8b1d527f78169
Zfex <55106553+Zfex@users.noreply.github.com>
2025-01-27 13:16:11 +0100

    Add files via upload

---
+++ App/Rover.Domain/userData.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain
+{
+    public class userData : IdentityUser
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string Id { get; set; }
+        public string emailUser { get; set; } 
+        public string telnumUser { get; set; } 
+        public string passwordUser { get; set; }
+        public string nameUser { get; set; }
+        public string surnameUser { get; set; }
+        public DateTime birthdayUser { get; set; }
+        public string sexUser { get; set; } 
+    }
+}
