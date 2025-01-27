8c1d3b85c7e176f8b42393b47bb21ee658969925
Zfex <55106553+Zfex@users.noreply.github.com>
2025-01-27 13:15:56 +0100

    Update Db.cs

--- App/Rover.Domain/Db.cs
+++ App/Rover.Domain/Db.cs
@@ -1,23 +1,46 @@
 namespace Rover.Domain
 {
-    public class User
+    public class userAddress
     {
-        public Guid Id { get; set; }
-        public string Name { get; set; }
-        public string Surname { get; set; }
-        public int PhoneNunber { get; set; }
-        public string Email { get; set; }
-        public string MainCity { get; set; }
-        public DateTime Birthday { get; set; }
+        public string Id { get; set; }
+        public int regionUser { get; set; }
+        public string cityUser { get; set; }
+        public int postCodeUser { get; set; }
+        public string streetUser { get; set; }
+        public int numApartUser { get; set; }
     }
 
-    public class Bike
+
+    public class bikeData
     {
-        public Guid BikeId { get; set; }
-        public string Brand { get; set; }
-        public string Model { get; set; }
-        public string Type { get; set; }
-        public double Price { get; set; }
-        public int Stock { get; set; }
+        public int IDbike { get; set; } 
+        public int IDmodel { get; set; } 
+        public int? IDstore { get; set; } 
+        public int? IDrepService { get; set; } 
+        public bool availableBike { get; set; } 
+        public string? IDreturnLocation { get; set; }
+    }
+    public class modelBike
+    {
+        public int IDmodel { get; set; }
+        public int nameModel { get; set; }
+        public string typeModel { get; set; }
+        public decimal priceModel { get; set; }
+        public string photoModel { get; set; }
+        public int amountBike { get; set; }
+        public int amountAvailable { get; set; }
+    }
+
+
+    public class orderUser
+    {
+        public int IDorder { get; set; }
+        public string Id { get; set; }
+        public int IDbike { get; set; }
+        public int IDpayment { get; set; }
+        public DateTime? totalTimeOrderStart { get; set; }
+        public DateTime? totalTimeOrderFinish { get; set; }
+        public DateTime? realTimeOrderStart { get; set; }
+        public DateTime? realTimeOrderFinish { get; set; }
     }
 }
