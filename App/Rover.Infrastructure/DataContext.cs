60331c73f4ec57f3d98f1b7dce0c0f1e44bba13d
Zfex <55106553+Zfex@users.noreply.github.com>
2025-01-27 13:16:56 +0100

    Update DataContext.cs

--- App/Rover.Infrastructure/DataContext.cs
+++ App/Rover.Infrastructure/DataContext.cs
@@ -1,21 +1,53 @@
-using Rover.Domain;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Rover.Domain;
+using Domain;
 
 namespace Rover.Infrastructure
 {
-    public class DataContext : DbContext
+    public class DataContext : IdentityDbContext<userData>
     {
-        public DataContext(DbContextOptions options) : base(options) { }
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
 
-        public DbSet<User> Users { get; set; }
-        public DbSet<Bike> Bikes { get; set; }
+        public DbSet<userAddress> userAddress { get; set; }
+        public DbSet<bikeData> bikeData { get; set; }
+        public DbSet<modelBike> modelBike { get; set; }
+        public DbSet<orderUser> orderUser { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            base.OnModelCreating(modelBuilder);
+            base.OnModelCreating(modelBuilder); // Important for IdentityDbContext
 
-            modelBuilder.Entity<User>().ToTable("Users");
-            modelBuilder.Entity<Bike>().ToTable("Bikes");
+            // Configure Identity tables
+            modelBuilder.Entity<userData>().ToTable("userData"); // Map Identity to "userData"
+
+            // Define custom primary keys
+            modelBuilder.Entity<userData>().HasKey(u => u.Id);
+            modelBuilder.Entity<userAddress>().HasKey(ua => ua.Id);
+            modelBuilder.Entity<bikeData>().HasKey(b => b.IDbike);
+            modelBuilder.Entity<modelBike>().HasKey(m => m.IDmodel);
+            modelBuilder.Entity<orderUser>().HasKey(o => o.IDorder);
+
+            // Define relationships
+            modelBuilder.Entity<userAddress>()
+                .HasOne<userData>()
+                .WithOne()
+                .HasForeignKey<userAddress>(ua => ua.Id);
+
+            modelBuilder.Entity<bikeData>()
+                .HasOne<modelBike>()
+                .WithMany()
+                .HasForeignKey(b => b.IDmodel);
+
+            modelBuilder.Entity<orderUser>()
+                .HasOne<userData>()
+                .WithMany()
+                .HasForeignKey(o => o.Id);
+
+            modelBuilder.Entity<orderUser>()
+                .HasOne<bikeData>()
+                .WithMany()
+                .HasForeignKey(o => o.IDbike);
         }
     }
 }
