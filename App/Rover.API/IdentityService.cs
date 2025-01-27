d7b5df7c00bb310b8ca8b4228f1699190632bd11
Zfex <55106553+Zfex@users.noreply.github.com>
2025-01-27 13:10:54 +0100

    Add files via upload

---
+++ App/Rover.API/IdentityService.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using Rover.Domain;
+using Rover.Infrastructure;
+using Domain;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Rover.API;
+
+public static class IdentityService
+{
+    public static IServiceCollection AddIdentityServices(this IServiceCollection services, IConfiguration _config)
+    {
+        services.AddIdentityCore<userData>(opt =>
+        {
+            opt.Password.RequireNonAlphanumeric = false;
+            opt.User.RequireUniqueEmail = true;
+        })
+            .AddEntityFrameworkStores<DataContext>();
+
+        // klucz musi być identyczny jak klucz szyfrujący w TokenService
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(/*"super tajne haslol23!!$super tajne haslol23!!$super tajne haslol23!!$super tajne haslol23!!$"*/_config["TokenKey"]));
+
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt =>
+        {
+            // jak walidować token
+            opt.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true, // Walidacja klucza podpisującego token
+                IssuerSigningKey = key, // Użycie wcześniej zdefiniowanego klucza symetrycznego do weryfikacji tokenu
+                ValidateIssuer = false, // Nie walidujemy wystawcy tokenu (Issuer)
+                ValidateAudience = false // Nie walidujemy odbiorcy tokenu (Audience)
+            };
+        });
+
+        // dodanie serwisu odpowiedzialnego za tworzenie tokenów
+        services.AddScoped<TokenService>();
+
+        return services;
+    }
+}
---
+++ App/Rover.API/TokenService.cs
@@ -0,0 +1,48 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Rover.Domain;
+using Domain;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Rover.API;
+
+public class TokenService
+{
+    private readonly IConfiguration _config;
+
+    public TokenService(IConfiguration config)
+    {
+        _config = config;
+    }
+    public string CreateToken(userData user)
+    {
+        if (user == null) throw new ArgumentNullException(nameof(user), "User object cannot be null.");
+        if (string.IsNullOrWhiteSpace(user.nameUser))
+            throw new ArgumentNullException(nameof(user.nameUser), "User name cannot be null or empty.");
+        if (string.IsNullOrWhiteSpace(user.emailUser))
+            throw new ArgumentNullException(nameof(user.emailUser), "User email cannot be null or empty.");
+
+        var claims = new List<Claim>
+    {
+        new Claim(ClaimTypes.Name, user.nameUser),
+        new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+        new Claim(ClaimTypes.Email, user.emailUser)
+    };
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["TokenKey"]));
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(claims),
+            Expires = DateTime.UtcNow.AddDays(7),
+            SigningCredentials = creds
+        };
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+
+        return tokenHandler.WriteToken(token);
+    }
+}
