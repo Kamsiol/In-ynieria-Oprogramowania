0862136f78b1f6f81c8cf4781d7b04e1480aa854
Zfex <55106553+Zfex@users.noreply.github.com>
2025-01-28 18:16:41 +0100

    Update Program.cs

--- App/Rover.API/Program.cs
+++ App/Rover.API/Program.cs
@@ -5,7 +5,6 @@ using Microsoft.EntityFrameworkCore;
 using Rover.Infrastructure;
 using System.Diagnostics;
 using Microsoft.AspNetCore.Identity;
-using Domain;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.Authorization;
 using Rover.API;
@@ -14,34 +13,32 @@ using MediatR;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Настройка аутентификации
+builder.Services.AddAuthentication("TestScheme")
+    .AddCookie("TestScheme", options =>
+    {
+        options.LoginPath = "/Account/Login"; // Задайте путь для логина, если потребуется
+        options.Cookie.Name = "TestAuthCookie";
+    });
+
 builder.Services.AddControllers(opt =>
 {
     var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
     opt.Filters.Add(new AuthorizeFilter(policy));
 });
 
-// Register MediatR
+// Регистрация MediatR
 builder.Services.AddMediatR(cfg =>
     cfg.RegisterServicesFromAssemblyContaining<BikeList>());
 
-// Register DbContext
+// Регистрация DbContext
 builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlServer(builder.Configuration.GetConnectionString("AzureDb"));
 });
 
-// Register Identity
-builder.Services.AddIdentity<userData, IdentityRole>()
-    .AddEntityFrameworkStores<DataContext>()
-    .AddDefaultTokenProviders();
-
-// Scoped logger service
 builder.Services.AddScoped(typeof(IAppLogger<>), typeof(AppLogger<>));
 
-builder.Services.AddScoped<TokenService>();
-
-// Configure CORS
 builder.Services.AddCors(opt =>
 {
     opt.AddPolicy("CorsPolicy", policy =>
@@ -52,10 +49,8 @@ builder.Services.AddCors(opt =>
     });
 });
 
-// Register FluentValidation
 builder.Services.AddFluentValidationAutoValidation();
 
-// Register Controllers and Swagger
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -73,18 +68,19 @@ app.UseCors("CorsPolicy");
 
 app.UseHttpsRedirection();
 
+// Подключение аутентификации и авторизации
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
 
-// Check and apply pending migrations
+// Проверка подключения к базе данных
 using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
 
 try
 {
     var context = services.GetRequiredService<DataContext>();
-    var userManager = services.GetRequiredService<UserManager<userData>>();
 
     await context.Database.MigrateAsync();
 }
