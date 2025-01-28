b96918424506e4d7cbe0d994e3bc8cc62721bbbc
Zfex <55106553+Zfex@users.noreply.github.com>
2025-01-28 18:18:58 +0100

    Add files via upload

---
+++ App/Rover.Application/RecerveRequest.cs
@@ -0,0 +1,21 @@
+﻿public class ReserveRequest
+{
+    public required int Id { get; set; }
+    public required int BikeId { get; set; }
+    public int IDpayment { get; set; }
+    public required DateTime StartTime { get; set; }
+    public required DateTime EndTime { get; set; }
+
+    public void Validate()
+    {
+        if (EndTime <= StartTime)
+        {
+            throw new ArgumentException("EndTime must be greater than StartTime.");
+        }
+
+        if (StartTime < DateTime.UtcNow)
+        {
+            throw new ArgumentException("StartTime must be in the future.");
+        }
+    }
+}
