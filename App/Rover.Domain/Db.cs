c265f565b0ffad76624b577864ecfa48c48256b0
Zfex <55106553+Zfex@users.noreply.github.com>
2025-01-28 18:19:27 +0100

    Update Db.cs

--- App/Rover.Domain/Db.cs
+++ App/Rover.Domain/Db.cs
@@ -1,8 +1,12 @@
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
 namespace Rover.Domain
 {
     public class userAddress
     {
-        public string Id { get; set; }
+        public int Id { get; set; }
         public int regionUser { get; set; }
         public string cityUser { get; set; }
         public int postCodeUser { get; set; }
@@ -13,11 +17,11 @@ namespace Rover.Domain
 
     public class bikeData
     {
-        public int IDbike { get; set; } 
-        public int IDmodel { get; set; } 
-        public int? IDstore { get; set; } 
-        public int? IDrepService { get; set; } 
-        public bool availableBike { get; set; } 
+        public int IDbike { get; set; }
+        public int IDmodel { get; set; }
+        public int? IDstore { get; set; }
+        public int? IDrepService { get; set; }
+        public bool availableBike { get; set; }
         public string? IDreturnLocation { get; set; }
     }
     public class modelBike
@@ -26,21 +30,31 @@ namespace Rover.Domain
         public int nameModel { get; set; }
         public string typeModel { get; set; }
         public decimal priceModel { get; set; }
-        public string photoModel { get; set; }
         public int amountBike { get; set; }
-        public int amountAvailable { get; set; }
     }
 
 
     public class orderUser
     {
+        [Key]
         public int IDorder { get; set; }
-        public string Id { get; set; }
+        public int Id { get; set; }
         public int IDbike { get; set; }
         public int IDpayment { get; set; }
         public DateTime? totalTimeOrderStart { get; set; }
         public DateTime? totalTimeOrderFinish { get; set; }
-        public DateTime? realTimeOrderStart { get; set; }
-        public DateTime? realTimeOrderFinish { get; set; }
+    }
+
+    public class userData
+    {
+        [Key]
+        public int Id { get; set; } // Убираем атрибут [DatabaseGenerated]
+        public string emailUser { get; set; }
+        public int telnumUser { get; set; }
+        public string passwordUser { get; set; }
+        public string nameUser { get; set; }
+        public string surnameUser { get; set; }
+        public DateTime birthdayUser { get; set; }
+        public string sexUser { get; set; }
     }
 }
